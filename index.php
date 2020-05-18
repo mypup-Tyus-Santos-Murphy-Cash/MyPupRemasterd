64bcccb028fe63389946bf148d9db922eb53df7e
Matt Murphy <murphy.d.matthew28@gmail.com>
2020-05-18 12:22:52 -0500

    updated

--- index.php
+++ index.php
@@ -4,8 +4,13 @@ require "header.php";
 
 
 <main>
-
-
+    <?php
+    if (isset($_SESSION['userId'])) {
+        echo '<p>You are logged in!</p>';
+    } else {
+        echo '<p>You are logged out!</p>';
+    }
+    ?>
 </main>
 
 
