20a95e204f82890a14b79f1570d8af8d42461f4e
Paris Tyus <Tyus.Paris1@gmail.com>
2020-05-18 11:22:45 -0500

    starting project

---
+++ header.php
@@ -0,0 +1,59 @@
+<?php
+session_start();
+?>
+
+<!doctype html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport"
+          content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title></title>
+    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.4.1/css/bootstrap.min.css" integrity="sha384-Vkoo8x4CGsO3+Hhxv8T/Q5PaXtkKtu6ug5TOeNV6gBiFeWPGFN9MuhOf23Q9Ifjh" crossorigin="anonymous">
+</head>
+<body>
+
+<header>
+    <nav class="navbar navbar-expand-lg navbar-light bg-light">
+        <a class="navbar-brand" href="#">Navbar</a>
+        <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
+            <span class="navbar-toggler-icon"></span>
+        </button>
+        <div class="collapse navbar-collapse" id="navbarNav">
+            <ul class="navbar-nav">
+                <li class="nav-item active">
+                    <a class="nav-link" href="index.php">Home <span class="sr-only">(current)</span></a>
+                </li>
+                <li class="nav-item">
+                    <a class="nav-link" href="#">Features</a>
+                </li>
+                <li class="nav-item">
+                    <a class="nav-link" href="#">Pricing</a>
+                </li>
+            <?php
+            if (isset($_SESSION['userId'])) {
+                echo '<form action="includes/logout.inc.php" method="post">
+            <button type="submit" name="logout-submit">Logout</button>
+        </form>';
+            } else {
+                echo '<li class="nav-item">
+            <form action="includes/login.inc.php" method="post">
+                <input type="text" name="mailuid" placeholder="username/email...">
+                <input type="password" name="pwd" placeholder="password...">
+                <button type="submit" name="login-submit">Login</button>
+            </form>
+        </li>
+        <li class="nav-item ml-3">
+            <a href="signup.php">Signup!</a>
+        </li>';
+            }
+
+            ?>
+
+
+            </ul>
+        </div>
+
+    </nav>
+</header>
