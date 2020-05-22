72a748669281ee2f6f3a4046a40e9f5d084ff13e
Matt Murphy <murphy.d.matthew28@gmail.com>
2020-05-22 12:17:41 -0500

    updated

--- signup.php
+++ signup.php
@@ -17,56 +17,60 @@ require "header.php";
                 <h5 class="card-title user-Form-Text">User Sign Up</h5>
                 <hr>
                 <form action="includes/signup.inc.php" method="POST">
-                    <div class="col-xl-12">
-                        <label for="userRole">Select User Role</label>
 
+<!--                    <div class="col-xl-12">-->
+<!--                        <label for="city">City</label>-->
+                        <input type="text" name="city" placeholder="city"/>
+<!--                    </div>-->
+
+<!--                    <div class="col-xl-12">-->
+<!--                        <label for="mail">Email</label>-->
+                        <input type="text" name="mail" placeholder="Email">
+<!--                    </div>-->
+
+<!--                    <div class="col-xl-12">-->
+<!--                        <label for="pwd">Password</label>-->
+                        <input type="password" name="pwd" placeholder="Password">
+<!--                    </div>-->
+
+<!--                    <div class="col-xl-12">-->
+<!--                        <label for="pwd-repeat"></label>-->
+                        <input type="password" name="pwd-repeat" placeholder="Re-enter password">
+<!--                    </div>-->
+
+<!--                    <div class="col-xl-12">-->
+<!--                        <label for="phoneNumber">Phone No.</label>-->
+                        <input type="text" name="phoneNumber" placeholder="phone number"/>
+<!--                    </div>-->
+
+<!--                    <div class="col-xl-12">-->
+<!--                        <label for="phoneNumber">Phone No.</label>-->
+                        <input type="text" name="image" placeholder="profile image"/>
+<!--                    </div>-->
+
+<!--                    <div class="col-xl-12">-->
+<!--                        <label for="state">State</label>-->
+                        <input type="text" name="state" placeholder="state"/>
+<!--                    </div>-->
+
+<!--                    <div class="col-xl-12">-->
+<!--                        <label for="userRole">Select User Role</label>-->
                         <select name="userRole">
                             <option value="breeder">Breeder</option>
                             <option value="buyer">Buyer</option>
 <!--                                                        <option value="admin">Admin</option>-->
                         </select>
-                    </div>
+<!--                    </div>-->
 
-                    <div class="col-xl-12">
-                        <label for="username">Username</label>
+<!--                    <div class="col-xl-12">-->
+<!--                        <label for="username">Username</label>-->
                         <input type="text" name="uid" placeholder="Username">
-                    </div>
+<!--                    </div>-->
 
-                    <div class="col-xl-12">
-                        <label for="pwd">Password</label>
-                        <input type="password" name="pwd" placeholder="Password">
-                    </div>
-
-                    <div class="col-xl-12">
-                        <label for="pwd-repeat"></label>
-                        <input type="password" name="pwd-repeat" placeholder="Re-enter password">
-                    </div>
-
-                    <div class="col-xl-12">
-                        <label for="mail">Email</label>
-                        <input type="text" name="mail" placeholder="Email">
-                    </div>
-
-                    <div class="col-xl-12">
-                        <label for="phoneNumber">Phone No.</label>
-                        <input type="text" name="phoneNumber" placeholder="phone number"/>
-                    </div>
-
-                    <div class="col-xl-12">
-                        <label for="city">City</label>
-                        <input type="text" name="city" placeholder="city"/>
-                    </div>
-
-                    <div class="col-xl-12">
-                        <label for="state">State</label>
-                        <input type="text" name="state" placeholder="state"/>
-                    </div>
-
-                    <div class="col-xl-12">
-                        <label for="zipcode">Zip Code</label>
+<!--                    <div class="col-xl-12">-->
+<!--                        <label for="zipcode">Zip Code</label>-->
                         <input type="text" name="zipCode" placeholder="zip code"/>
-                    </div>
-
+<!--                    </div>-->
 
 <!--                    <div class="col-xl-12">-->
 <!--                        <button class="button photo-upload upload-photo" type="button" id="picker">Profile Image-->
