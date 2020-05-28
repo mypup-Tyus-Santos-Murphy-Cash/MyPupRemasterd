802ffa493a394bcccd83868a64789866c9602bb4
Matt Murphy <murphy.d.matthew28@gmail.com>
2020-05-28 11:47:58 -0500

    updated

--- signup.php
+++ signup.php
@@ -16,32 +16,28 @@ require "header.php";
             <div class="card-body sign-up-form">
                 <h5 class="card-title user-Form-Text">User Sign Up</h5>
                 <hr>
-
-                <form action="includes/signup.inc.php" method="POST">
-                    <input type="text" name="uid" placeholder="Username"/>
-                        <input type="text" name="mail" placeholder="Email"/>
-                            <input type="password" name="pwd" placeholder="Password"/>
-                                <input type="password" name="pwd-repeat" placeholder="Re-enter password"/>
-                                    <input type="text" name="phone-number" placeholder="phone number"/>
-                                        <input type="text" name="city" placeholder="city"/>
-                                    <input type="text" name="state" placeholder="state"/>
-                                <input type="text" name="zipcode" placeholder="zipcode"/>
-                            <select name="user-role">
-                                <option value="breeder">Breeder</option>
-                                    <option value="buyer">Buyer</option>
-                                <option value="admin">Admin</option>
-                            </select>
-                        <input type="text" name="image" placeholder="image">
-                    <button type="submit" name="signup-submit">Sign me up!</button>
-                </form>
-
+                        <form action="includes/signup.inc.php" method="POST">
+                            <input type="text" name="uid" placeholder="Username"/>
+                                <input type="text" name="mail" placeholder="Email"/>
+                                    <input type="password" name="pwd" placeholder="Password"/>
+                                        <input type="password" name="pwd-repeat" placeholder="Re-enter password"/>
+                                            <input type="text" name="phone-number" placeholder="phone number"/>
+                                                <input type="text" name="city" placeholder="city"/>
+                                            <input type="text" name="state" placeholder="state"/>
+                                        <input type="text" name="zipcode" placeholder="zipcode"/>
+                                    <select name="user-role">
+                                        <option value="breeder">Breeder</option>
+                                            <option value="buyer">Buyer</option>
+                                        <option value="admin">Admin</option>
+                                    </select>
+                                <input type="text" name="image" placeholder="image">
+                            <button type="submit" name="signup-submit">Sign me up!</button>
+                        </form>
+                    </div>
+                </div>
             </div>
-
         </div>
-    </div>
-
-</div>
-</main>
+    </main>
 
 <?php
 require "footer.php";
