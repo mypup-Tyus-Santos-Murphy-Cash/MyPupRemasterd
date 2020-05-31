3468887571b6967e97e1cc5fe99a5a03a5a940a3
Matt Murphy <murphy.d.matthew28@gmail.com>
2020-05-31 02:26:19 -0500

    updated

--- migration.sql
+++ migration.sql
@@ -1,29 +1,5 @@
--- MySQL Workbench Forward Engineering
 
-SET @OLD_UNIQUE_CHECKS=@@UNIQUE_CHECKS, UNIQUE_CHECKS=0;
-SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0;
-SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='ONLY_FULL_GROUP_BY,STRICT_TRANS_TABLES,NO_ZERO_IN_DATE,NO_ZERO_DATE,ERROR_FOR_DIVISION_BY_ZERO,NO_ENGINE_SUBSTITUTION';
-
--- -----------------------------------------------------
--- Schema mydb
--- -----------------------------------------------------
-
--- -----------------------------------------------------
--- Schema adlister_db
--- -----------------------------------------------------
-
--- -----------------------------------------------------
--- Schema adlister_db
--- -----------------------------------------------------
-
-CREATE SCHEMA IF NOT EXISTS `mypupRemasterd_db` DEFAULT CHARACTER SET utf8 ;
-USE `mypupRemasterd_db` ;
-
--- -----------------------------------------------------
--- Table `adlister_db`.`users`
--- -----------------------------------------------------
-
-CREATE TABLE IF NOT EXISTS `mypupRemasterd_db`.`users` (
+CREATE TABLE `users` (
   `id` INT(11) UNSIGNED NOT NULL AUTO_INCREMENT,
   `username` VARCHAR(255) NOT NULL,
   `email` VARCHAR(255) NOT NULL,
@@ -36,16 +12,10 @@ CREATE TABLE IF NOT EXISTS `mypupRemasterd_db`.`users` (
   `image` VARCHAR(20) NOT NULL,
   PRIMARY KEY (`id`),
   UNIQUE INDEX `username_UNIQUE` (`username` ASC),
-  UNIQUE INDEX `id_UNIQUE` (`id` ASC))
-ENGINE = InnoDB
-AUTO_INCREMENT = 11
-DEFAULT CHARACTER SET = utf8;
+  UNIQUE INDEX `id_UNIQUE` (`id` ASC));
 
--- -----------------------------------------------------
--- Table `adlister_db`.`ads`
--- -----------------------------------------------------
 
-CREATE TABLE IF NOT EXISTS `mypupRemasterd_db`.`dog_post` (
+CREATE TABLE `dog_post` (
   `id` INT(10) UNSIGNED NOT NULL AUTO_INCREMENT,
   `user_id` INT(10) UNSIGNED NULL DEFAULT NULL,
   `dog_breed` VARCHAR(200) NOT NULL,
@@ -59,65 +29,52 @@ CREATE TABLE IF NOT EXISTS `mypupRemasterd_db`.`dog_post` (
     FOREIGN KEY (`user_id`)
     REFERENCES `mypupRemasterd_db`.`users` (`id`)
     ON DELETE CASCADE
-    ON UPDATE CASCADE)
-ENGINE = InnoDB
-AUTO_INCREMENT = 6
-DEFAULT CHARACTER SET = utf8;
+    ON UPDATE CASCADE);
 
-
--- -----------------------------------------------------
--- Table `adlister_db`.`categories`
--- -----------------------------------------------------
-
-CREATE TABLE IF NOT EXISTS `mypupRemasterd_db`.`breed` (
+CREATE TABLE `breed` (
   `id` INT UNSIGNED NOT NULL AUTO_INCREMENT,
   `name` VARCHAR(200) NOT NULL,
   PRIMARY KEY (`id`),
   UNIQUE INDEX `id_UNIQUE` (`id` ASC),
-  UNIQUE INDEX `name_UNIQUE` (`name` ASC))
-ENGINE = InnoDB;
+  UNIQUE INDEX `name_UNIQUE` (`name` ASC));
 
 
--- -----------------------------------------------------
--- Table `adlister_db`.`ads_has_category`
--- -----------------------------------------------------
-
-CREATE TABLE IF NOT EXISTS `mypupRemasterd_db`.`dog_post_has_breed` (
+CREATE TABLE `dog_post_has_breed` (
   `dog_post_id` INT(10) UNSIGNED NOT NULL,
   `breed_id` INT UNSIGNED NOT NULL,
   PRIMARY KEY (`dog_post_id`, `breed_id`),
   INDEX `fk_dog_post_has_breed_breed1_idx` (`breed_id` ASC),
   INDEX `fk_dog_post_has_breed_dog_post1_idx` (`dog_post_id` ASC),
-  CONSTRAINT `fk_dog_post_has_breed_dog_post1` FOREIGN KEY (`dog_post_id`) REFERENCES `mypupRemasterd_db`.`dog_post` (`id`)
+  CONSTRAINT `fk_dog_post_has_breed_dog_post1`
+  FOREIGN KEY (`dog_post_id`)
+  REFERENCES `mypupRemasterd_db`.`dog_post` (`id`)
     ON DELETE CASCADE
     ON UPDATE CASCADE,
-  CONSTRAINT `fk_dog_post_has_breed_breed1` FOREIGN KEY (`breed_id`) REFERENCES `mypupRemasterd_db`.`breed` (`id`)
+  CONSTRAINT `fk_dog_post_has_breed_breed1`
+  FOREIGN KEY (`breed_id`)
+  REFERENCES `mypupRemasterd_db`.`breed` (`id`)
     ON DELETE CASCADE
-    ON UPDATE CASCADE)
-ENGINE = InnoDB
-DEFAULT CHARACTER SET = utf8;
+    ON UPDATE CASCADE);
 
-CREATE TABLE IF NOT EXISTS `mypupRemasterd_db`.`breed_selection` (
+CREATE TABLE `breed_selection` (
   `user_id` INT(10) UNSIGNED NOT NULL,
   `breed_id` INT UNSIGNED NOT NULL,
   PRIMARY KEY (`user_id`, `breed_id`),
   INDEX `fk_users_has_breed_breed1_idx` (`breed_id` ASC),
   INDEX `fk_users_has_breed_users1_idx` (`user_id` ASC),
-  CONSTRAINT `fk_users_has_breed_users1` FOREIGN KEY (`user_id`) REFERENCES `mypupRemasterd_db`.`users` (`id`)
+  CONSTRAINT `fk_users_has_breed_users1`
+  FOREIGN KEY (`user_id`)
+  REFERENCES `mypupRemasterd_db`.`users` (`id`)
     ON DELETE CASCADE
     ON UPDATE CASCADE,
-  CONSTRAINT `fk_users_has_breed_breed1` FOREIGN KEY (`breed_id`) REFERENCES `mypupRemasterd_db`.`breed` (`id`)
+  CONSTRAINT `fk_users_has_breed_breed1`
+  FOREIGN KEY (`breed_id`)
+  REFERENCES `mypupRemasterd_db`.`breed` (`id`)
     ON DELETE CASCADE
-    ON UPDATE CASCADE)
-ENGINE = InnoDB
-DEFAULT CHARACTER SET = utf8;
+    ON UPDATE CASCADE);
 
 
--- -----------------------------------------------------
--- Table `adlister_db`.`favorites`
--- -----------------------------------------------------
-
-CREATE TABLE IF NOT EXISTS `mypupRemasterd_db`.`favorites` (
+CREATE TABLE `favorites` (
   `user_id` INT(10) UNSIGNED NOT NULL,
   `dog_post_id` INT(10) UNSIGNED NOT NULL,
   PRIMARY KEY (`user_id`, `dog_post_id`),
@@ -132,11 +89,5 @@ CREATE TABLE IF NOT EXISTS `mypupRemasterd_db`.`favorites` (
     FOREIGN KEY (`dog_post_id`)
     REFERENCES `mypupRemasterd_db`.`dog_post` (`id`)
     ON DELETE CASCADE
-    ON UPDATE CASCADE)
-ENGINE = InnoDB
-DEFAULT CHARACTER SET = utf8;
+    ON UPDATE CASCADE);
 
-
-SET SQL_MODE=@OLD_SQL_MODE;
-SET FOREIGN_KEY_CHECKS=@OLD_FOREIGN_KEY_CHECKS;
-SET UNIQUE_CHECKS=@OLD_UNIQUE_CHECKS;
